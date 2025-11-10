04485160dafedd195e756459980c1892f67f32dd
Angela Carvalho <angelalopo@outlook.com>
2025-11-10 18:49:41 -0300

    fix: update GCS configuration to conditionally use keyFilename based on environment variable

--- src/config/gcs.js
+++ src/config/gcs.js
@@ -1,10 +1,12 @@
 import { Storage } from '@google-cloud/storage';
 import 'dotenv/config';
 
-const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  credentials: JSON.parse(process.env.GCP_CREDENTIALS), 
-});
+const storage = process.env.GCP_KEY_FILE_PATH
+  ? new Storage({
+      projectId: process.env.GCP_PROJECT_ID,
+      keyFilename: process.env.GCP_KEY_FILE_PATH,
+    })
+  : new Storage(); 
 
 const bucket = storage.bucket(process.env.GCP_BUCKET_NAME);
 
